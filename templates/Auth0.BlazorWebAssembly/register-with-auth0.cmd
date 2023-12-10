cce978cadaaf74b7837aee2442d306ea775286cb
Andrea Chiarelli <25705235+andychiare@users.noreply.github.com>
2023-12-10 15:18:01 +0100

    Fix shebang issue

--- templates/Auth0.BlazorWebAssembly/register-with-auth0.cmd
+++ templates/Auth0.BlazorWebAssembly/register-with-auth0.cmd
@@ -1,3 +1,8 @@
+rem #if (OS != "Windows_NT")
 #!/bin/sh
 dotnet ./Client/registration/cli-wrapper.dll
 dotnet ./Server/registration/cli-wrapper.dll
+rem #else
+dotnet Client/registration/cli-wrapper.dll
+dotnet Server/registration/cli-wrapper.dll
+rem #endif
