e012ca84a4a3a5cd2743cadfb4ec2294ffcbf8aa
Andrea Chiarelli <25705235+andychiare@users.noreply.github.com>
2024-05-03 18:26:06 +0200

    Add MAUI template

---
+++ templates/Auth0.Maui/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Auth0.OidcClient; 
+
+namespace Auth0Maui;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+	private readonly Auth0Client auth0Client;
+
+	public MainPage(Auth0Client client)
+	{
+		InitializeComponent();
+		auth0Client = client;
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	private async void OnLoginClicked(object sender, EventArgs e)
+	{
+		var loginResult = await auth0Client.LoginAsync();
+
+		if (!loginResult.IsError)
+		{
+			UsernameLbl.Text = loginResult.User.Identity.Name;
+      		UserPictureImg.Source = loginResult.User
+		        .Claims.FirstOrDefault(c => c.Type == "picture")?.Value;
+
+			LoginView.IsVisible = false;
+			HomeView.IsVisible = true;
+		}
+		else
+		{
+		await DisplayAlert("Error", loginResult.ErrorDescription, "OK");
+		}
+	}
+
+	private async void OnLogoutClicked(object sender, EventArgs e)
+	{
+		var logoutResult = await auth0Client.LogoutAsync();
+
+		HomeView.IsVisible = false;
+		LoginView.IsVisible = true;
+	}
+}
+
---
+++ templates/Auth0.Maui/Platforms/Android/WebAuthenticationCallbackActivity.cs
@@ -0,0 +1,16 @@
+using Android.App;
+using Android.Content.PM;
+
+namespace MauiAuth0App;
+
+[Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+[IntentFilter(new[] { Android.Content.Intent.ActionView },
+              Categories = new[] { 
+                Android.Content.Intent.CategoryDefault,
+                Android.Content.Intent.CategoryBrowsable 
+              },
+              DataScheme = CALLBACK_SCHEME)]
+public class WebAuthenticationCallbackActivity : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
+{
+    const string CALLBACK_SCHEME = "myapp";
+}
