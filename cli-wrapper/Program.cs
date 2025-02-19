a8ee9a924fe94b77af096fc33927881739c65d43
Andrea Chiarelli <25705235+andychiare@users.noreply.github.com>
2025-02-19 12:38:05 +0100

    Add warning message to the registration step

--- cli-wrapper/Program.cs
+++ cli-wrapper/Program.cs
@@ -5,6 +5,10 @@
 if (await cliWrapper.IsAuth0CliInstalled())
 {
   Console.WriteLine("Auth0 CLI is installed.");
+  Console.WriteLine("Before starting the registration step, make sure you are logged in to your Auth0 tenant through Auth0 CLI.");
+  Console.WriteLine("If you are not logged in, open a new terminal window and run `auth0 login`, then come back here to continue the registration step.");
+  Console.WriteLine("If you are already logged in, press any key to continue...");
+  Console.ReadKey();
 
   var registrationData = await cliWrapper.Register();
 
