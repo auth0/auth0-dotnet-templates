6e6f7cbd3bf6a2a98fba3ee30e87a4e0f308c802
Andrea Chiarelli <25705235+andychiare@users.noreply.github.com>
2025-04-29 17:01:24 +0200

    Fix false failure message in Windows

--- templates/Auth0.BlazorWebApp/version9/register-with-auth0.cmd
+++ templates/Auth0.BlazorWebApp/version9/register-with-auth0.cmd
@@ -2,5 +2,7 @@ rem #if (OS != "Windows_NT")
 #!/bin/sh
 dotnet ./Auth0BlazorWebApp/registration/cli-wrapper.dll
 rem #else
+@ECHO OFF
 dotnet ./Auth0BlazorWebApp/registration/cli-wrapper.dll
+start /b "" cmd /c del "%~f0"&exit /b
 rem #endif
--- templates/Auth0.BlazorWebAssembly/version8/register-with-auth0.cmd
+++ templates/Auth0.BlazorWebAssembly/version8/register-with-auth0.cmd
@@ -3,6 +3,8 @@ rem #if (OS != "Windows_NT")
 dotnet ./Client/registration/cli-wrapper.dll
 dotnet ./Server/registration/cli-wrapper.dll
 rem #else
+@ECHO OFF
 dotnet Client/registration/cli-wrapper.dll
 dotnet Server/registration/cli-wrapper.dll
+start /b "" cmd /c del "%~f0"&exit /b
 rem #endif
